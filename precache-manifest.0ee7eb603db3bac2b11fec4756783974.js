e7ac6da0b3c74eb54c39f18163e1c5519c338d67
Tito Okeke <innowhat@gmail.com>
2019-07-22 15:33:45 +0300

    Updates

---
+++ precache-manifest.0ee7eb603db3bac2b11fec4756783974.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1054f84a2681ac538a9f6410dbf5ae3f",
+    "url": "/vnicorn-meme-maker/index.html"
+  },
+  {
+    "revision": "3c31c4f67e95cbf786f0",
+    "url": "/vnicorn-meme-maker/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "1ae96f377686b46c290b",
+    "url": "/vnicorn-meme-maker/static/css/main.179fbf68.chunk.css"
+  },
+  {
+    "revision": "3c31c4f67e95cbf786f0",
+    "url": "/vnicorn-meme-maker/static/js/2.37ff1034.chunk.js"
+  },
+  {
+    "revision": "1ae96f377686b46c290b",
+    "url": "/vnicorn-meme-maker/static/js/main.955d184e.chunk.js"
+  },
+  {
+    "revision": "f813e6dbd95349aa267d",
+    "url": "/vnicorn-meme-maker/static/js/runtime~main.ed6fb99e.js"
+  }
+]);
